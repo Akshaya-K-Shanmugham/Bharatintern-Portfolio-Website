1711868d59f2a26e9fbb23279ab45eca09918816
Akshaya-K-Shanmugham <156498605+Akshaya-K-Shanmugham@users.noreply.github.com>
2024-06-26 20:09:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Software Developer", "Web Developer", "Python Developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
